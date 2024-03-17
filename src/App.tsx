5e22bff0ab2032b98d63d8145187afba40eb8b90
Aleena_Antony <aleena@bluelogic.ae>
2024-03-17 20:39:14 +0400

    file path modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MovieList from "./Pages/MovieList/MovieList";
-import MovieDetails from "./Pages/MovieDetails/MovieDetails";
-import PageNotFound from "./Pages/PageNotFound/PageNotFound";
-import MoviePage from "./Pages/WelcomePage/WelcomePage";
+import MovieList from "./pages/MovieList/MovieList";
+import MovieDetails from "./pages/MovieDetails/MovieDetails";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import MoviePage from "./pages/WelcomePage/WelcomePage";
 import Layout from "./Layout/Layout";
 
 const App = () => {
